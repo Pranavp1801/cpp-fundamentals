5ef4eb7c3e8d42c6ffe7a80f4ca1e5da4dc69484
Pranavp1801 <ppawar1801@gmail.com>
2025-12-26 17:11:52 +0530

    Add loops and variables in c++

---
+++ loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Printing numbers from 1 to 5:" << endl;
+
+    for(int number = 1; number <= 5; number++) {
+        cout << number << endl;
+    }
+
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int student_rollno = 1; //stores whole number
+    float attendance = 80.5; //stores decimal
+    string name="Pranav"; //stores strings
+    char grade='A'; //stores single character
+    bool isPass=true; // stores true or false
+    
+    cout << "Roll Number : "<<student_rollno<<endl;
+    cout << "Student Name : "<<name<<endl;
+    cout << "Attendance : "<<attendance<<endl;
+    cout << "Grade: "<<grade<<endl;
+    cout << "Passing Status: "<<isPass<<endl;
+    return 0;
+}
