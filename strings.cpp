2715f83f46cc3c74d5ac1078fc1186c8f3de8e9d
Pranavp1801 <ppawar1801@gmail.com>
2025-12-26 17:15:04 +0530

    Add string example

---
+++ strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string studentName = "Pranav";
+
+    cout << "Student name is: " << studentName << endl;
+    cout << "Length of name: " << studentName.length() << endl;
+
+    return 0;
+}
